b155ba637b39706b5b0ea947c566b72ba4281539
gamboaMirella <mgamboav@unitru.edu.pe>
2024-06-18 23:21:58 -0500

    Commit

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,9 +1,9 @@
 <ul>
   <li>
-    <a href="{{route('inicio')}}">Home</a>
-    <a href="{{route('programas.index')}}">Contacto</a> 
-    <a href="{{route('talleres.index')}}">Servicios</a>
-    <a href="{{route('asesoramiento')}}">Nosotros</a>
+    <a href="{{route('inicio')}}">Inicio</a>
+    <a href="{{route('programas.index')}}">Programas</a> 
+    <a href="{{route('talleres.index')}}">Talleres</a>
+    <a href="{{route('asesoramiento')}}">Asesoramiento</a>
     <a href="{{route('contacto')}}">Contacto</a>       
   </li>
 </ul>
---
+++ resources/views/talleres/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts/layout')
+
+@section('title','Talleres')
+
+@section('content')
+    <h1>Talleres</h1>
+@endsection
